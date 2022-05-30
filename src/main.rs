0b11a0b60db6f6b7e118515bd8dd82a9634116d3
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-30 01:22:54 -0400

    add area calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let width: u32 = 10;
+    let height: u32 = 15;
+
+    let rectangle_area = area(width, height);
+
+    println!("The rectangle has an area of {}", rectangle_area);
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
